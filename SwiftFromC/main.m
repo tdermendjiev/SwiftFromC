55040812b16ebca60690b3a2ec6dd98ed56c974a
Dermendzhiev, Teodor (external - Project) <tdermendjievft@gmail.com>
2022-05-27 16:52:16 +0300

    Initial Commit

---
+++ SwiftFromC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SwiftFromC
+//
+//  Created by Dermendzhiev, Teodor (external - Project) on 27.05.22.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
